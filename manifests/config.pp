36534d69b40b1d6c6956630da904d073ed55cfa7
Davíð Örn Jóhannsson <davidoj@siminn.is>
2017-01-20 15:27:02 +0000

    adding coreos support

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class etcd::config {
     content => template("${module_name}/etc/etcd/etcd.conf.erb"),
   }
 
-  if $::etcd::manage_package and $::etcd::journald_forward_enable and $::operatingsystemmajrelease == '7' {
+  if $::etcd::manage_package and $::etcd::journald_forward_enable and ($::operatingsystemmajrelease == '7' or $::osfamily == 'CoreOS') {
     file { '/etc/systemd/system/etcd.service.d':
       ensure => 'directory',
       owner  => 'root',
